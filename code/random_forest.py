44e5d71ec1b894c6a5622890179a2fe898bd90ff
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2026-04-09 14:07:25 -0400

    pre-fine-tuned random forest

--- code/random_forest.py
+++ code/random_forest.py
@@ -3,8 +3,8 @@ import numpy as np
 from TreeNode import TreeNode, DecideLow, DecideMedium, DecideHigh
 
 def load_train_data():
-    x_train = pd.read_csv('data/X_train_scaled.csv')
-    y_train = pd.read_csv('data/y_train.csv')
+    x_train = pd.read_csv('data/processed_data/X_train_scaled.csv')
+    y_train = pd.read_csv('data/processed_data/y_train.csv')
     features = x_train.columns
     return x_train, y_train, features
 
@@ -32,20 +32,22 @@ def entropy(classes: np.ndarray):
     prob_2 = len(classes[classes == 2]) / len(classes)
     return - (prob_1*log2(prob_1) + prob_2*log2(prob_2) + prob_0*log2(prob_0))
     
-def best_avg_entropy(data: pd.DataFrame, target: np.ndarray,feature_name: str):
+def best_avg_entropy(data: pd.DataFrame, target: np.ndarray, feature_name: str):
     """
     Finds the minimum average entropy in the data when splitting on the given feature and returns that value and the threshold at which it occurs.
     """
     feature = np.array(data[feature_name])
+    
     avg_entropies = []
-    for thresh in range(np.min(feature), np.max(feature)+1):
+    thresh_range = np.linspace(np.min(feature), np.max(feature), num=50)
+    for thresh in thresh_range:
         under_classes = target[feature <= thresh]
         over_classes = target[feature > thresh]
         avg_entropies.append(entropy(under_classes)*(len(under_classes)/len(target)) + entropy(over_classes)*(len(over_classes)/len(target)))
     np_entropies = np.array(avg_entropies)
-    return np.min(np_entropies), np.argmin(np_entropies)+np.min(feature)
+    return np.min(np_entropies), thresh_range[np.argmin(np_entropies)]
         
-def construct_node(data: pd.DataFrame, target: np.ndarray,parent_level: int):
+def construct_node(data: pd.DataFrame, target: np.ndarray, features, parent_level: int):
     """
     Recursively construct a decision tree using a custom node structure.
     """
@@ -54,24 +56,25 @@ def construct_node(data: pd.DataFrame, target: np.ndarray,parent_level: int):
     # training data has fewer than 23 samples in this node, or more than 85% of
     # the data belongs to one class, return a node representing the decision of 
     # either -1 or 1
-    pct_0 = len(target[target == 0]) / len(target)
-    pct_1 = len(target[target == 1]) / len(target)
-    pct_2 = len(target[target == 2]) / len(target)
-    if (pct_0 > 0.85):
-        return DecideLow(parent_level+1)
-    elif (pct_1 > 0.85):
-        return DecideMedium(parent_level+1)
-    elif (pct_2 > 0.85):        
-        return DecideHigh(parent_level+1)
-    if (parent_level == 4 or len(data) < 23):
-        
+    decisions = {
+        0: DecideLow(parent_level+1),
+        1: DecideMedium(parent_level+1),
+        2: DecideHigh(parent_level+1)
+    }
+    if (parent_level == 5 or len(data) < 23):
+        if len(data) == 0:
+            return DecideLow(parent_level+1)
+        return decisions[np.argmax(np.bincount(target))]
+    for i in range(3):
+        if len(target[target == i]) / len(target) > 0.85:
+            return decisions[i] 
         
     # Decide which attribute is the best to split on and at which threshold
     best_entropy = 1
     best_thresh = 0
     best_att = ""
-    for att in ATTRIBUTES:
-        ent, thresh = best_avg_entropy(data, att)
+    for att in features:
+        ent, thresh = best_avg_entropy(data, target, att)
         if ent < best_entropy:
             best_entropy = ent
             best_thresh = thresh
@@ -81,17 +84,33 @@ def construct_node(data: pd.DataFrame, target: np.ndarray,parent_level: int):
     # function to construct more nodes for splitting the data on each side of 
     # the current threshold.
     this_node = TreeNode(best_att, best_thresh, parent_level+1)
-    this_node.under = construct_node(data[data[best_att] <= best_thresh], parent_level+1)
-    this_node.over = construct_node(data[data[best_att] > best_thresh], parent_level+1)
+    this_node.under = construct_node(data[data[best_att] <= best_thresh], target[data[best_att] <= best_thresh], features, parent_level+1)
+    this_node.over = construct_node(data[data[best_att] > best_thresh], target[data[best_att] > best_thresh], features, parent_level+1)
     return this_node
 
-def build_tree(x, y, max_depth=None):
-    
+def classify(forest: list[TreeNode], x: pd.DataFrame):
+    y = []
+    for sample in x.iterrows():
+        bins = [0, 0, 0]
+        for tree in forest:
+            bins[tree.decide(sample[1])] += 1
+        y.append(np.argmax(bins))
+        
+    return np.array(y)
 
 def main():
     # Load the training data
     x_train, y_train, features = load_train_data()
 
+    # Make the random forest
+    trees = []
+    for _ in range(20):
+        xb, yb = bootstrap_sample(x_train, y_train)
+        trees.append(construct_node(xb, yb["burnout_risk"], features, 0))
+
+    # Run!
+    y_train_pred = classify(trees, x_train)
+    print(len(y_train_pred[y_train_pred == y_train["burnout_risk"]]) / len(y_train))
 
 if __name__ == "__main__":
     main()
